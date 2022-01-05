5ef97f311a535a9d295c88b1e68be65fb959ccd8
Your Name <you@example.com>
2022-01-05 03:02:54 -0800

    first commit

---
+++ model/database.php
@@ -0,0 +1,12 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=assignment_tracker';
+    $username = 'root'
+
+    try {
+        $db = new PDO($dsn, $username);
+    } catch (PDOException $e) {
+        $error = 'Database Error: ';
+        $error .= $e->getMessage();
+        include('view/error.php');
+        exit();
+    }
---
+++ view/error.php
@@ -0,0 +1,5 @@
+<?php include('header.php') ?>
+<h2>Error</h2>
+<p><?= $error ?></p> 
+<br>
+<?php include('footer.php') ?>
